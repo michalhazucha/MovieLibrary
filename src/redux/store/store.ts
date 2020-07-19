adf5b0e148e056381c203950d96d4ad91cc86fa5
Michal Hazucha <hazuchamichal@gmail.com>
2020-07-19 12:38:26 +0200

    Local Storage Implemented

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,12 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers/rootReducer';
+import { createStore, applyMiddleware,compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { saveState, loadState } from '../localStorage/localStorage';
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import rootSaga from '../sagas/movieSaga';
 const sagaMiddleware = createSagaMiddleware();
-
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
 export default store;
