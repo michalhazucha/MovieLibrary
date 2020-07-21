b9623c338179bd09454d32b8e12b3bf276def599
Michal Hazucha <hazuchamichal@gmail.com>
2020-07-21 05:38:44 +0200

    Life Before Local Storage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect,ChangeEvent } from 'react';
+import React, { useEffect, ChangeEvent } from 'react';
+import {IMovie} from '../../interfaces'
 import { useSelector, useDispatch } from 'react-redux';
 import { addFavourite, getNameAction,setNameAction,toggleFavAction } from '../../redux/actions/actionCreators/movieActionCreators';
 import { ActionTypes } from '../../redux/actions/types';
@@ -24,7 +25,23 @@ function App() {
         <input type="text" onChange={handleSetName}  ></input>
       <button onClick={handleSearch}>searc</button>
       <button onClick={handleToggle}>Toggle</button>
-        {movie.Title}
+      <div>
+        <img src={movie.Poster} alt={movie.Name}/>
+        <h1>{movie.Title}</h1>
+      </div>
+      Favourites
+      <div>
+        {favourites.map((fav: IMovie) => (
+          <div>
+            <img src={fav.Poster} alt={fav.Title} />
+            <h1>{fav.Title}</h1>
+          </div>
+        ))}
+
+
+      </div>
+
+        
     </div>
   );
 }
