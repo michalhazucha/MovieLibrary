520fbfeaba39f1f0dd4d41c4221065b02d0275dd
Michal Hazucha <hazuchamichal@gmail.com>
2020-07-26 11:20:08 +0200

    eslint & prettier included

--- src/components/AppBar/Searcher/index.tsx
+++ src/components/AppBar/Searcher/index.tsx
@@ -1,35 +1,28 @@
-import React,{ChangeEvent,useState} from 'react'
+import React, { ChangeEvent, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {AppState} from '../../../redux/reducers/rootReducer'
+import { AppState } from '../../../redux/reducers/rootReducer';
 import { getNameAction, setNameAction } from '../../../redux/actions/actionCreators/movieActionCreators';
 import { useHistory } from 'react-router-dom';
 //styles
 import { Input } from 'antd';
 const { Search } = Input;
 const Searcher = () => {
-  const dispatch = useDispatch(); 
-  const {favourites } = useSelector((state: AppState) => state.movie);
+  const dispatch = useDispatch();
+  const { favourites } = useSelector((state: AppState) => state.movie);
   const history = useHistory();
   const [movieName, setMovieName] = useState();
-  
+
   const navigateTo = () => history.push('/');
-  const handleSetName = (e: ChangeEvent<HTMLInputElement>|any) => {
+  const handleSetName = (e: ChangeEvent<HTMLInputElement> | any) => {
     dispatch(setNameAction(e));
   };
   const handleSearch = () => {
     dispatch(getNameAction(movieName, favourites));
     handleSetName(movieName);
-     navigateTo();   
+    navigateTo();
   };
 
-   return (
-     <Search
-       placeholder="input search text"
-       onChange={(e)=>setMovieName(e.target.value)}
-      onSearch={handleSearch}
-      style={{ border: 'none' }}
-    />
-  )
-}
+  return <Search placeholder="input search text" onChange={(e) => setMovieName(e.target.value)} onSearch={handleSearch} style={{ border: 'none' }} />;
+};
 
-export default Searcher
+export default Searcher;
--- src/components/MovieInfo/index.tsx
+++ src/components/MovieInfo/index.tsx
@@ -1,42 +1,45 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../redux/reducers/rootReducer';
 import Movie from './Movie';
 import { Row, Space, Typography } from 'antd';
 import './MovieInfo.scss';
-const {Title} =Typography
+const { Title } = Typography;
 const MovieInfo = () => {
   const { movie } = useSelector((state: AppState) => state.movie);
   const { name } = useSelector((state: AppState) => state.movie);
-  if (movie.Response=== 'True') {
+  if (movie.Response === 'True') {
     return (
       <Row>
-        <Movie
-          Title={movie.Title}
-          Poster={movie.Poster}
-          Plot={movie.Plot}
-          Type={movie.Type}
-          Director={movie.Director}
-          Writer={movie.Writer}
-          Actors={movie.Actors}
-          Awards={movie.Awards}
-          imdbID={movie.imdbID}
-        />
-      </Row>)
-  } else if (movie.Response==='False' && name !== '') {
+        <Movie Title={movie.Title} Poster={movie.Poster} Plot={movie.Plot} Type={movie.Type} Director={movie.Director} Writer={movie.Writer} Actors={movie.Actors} Awards={movie.Awards} imdbID={movie.imdbID} />
+      </Row>
+    );
+  } else if (movie.Response === 'False' && name !== '') {
     return (
-    <Row> <Space direction="vertical">
-        <Title level={1} className="text-welcome text-center" > Ooops! <span>😯</span> This movie was not found</Title>
-        <Title level={3}>Please check if you typed your movie Title Correctly or search for another movie <span>😊</span> </Title>
-      </Space></Row>)
-    
+      <Row>
+        {' '}
+        <Space direction="vertical">
+          <Title level={1} className="text-welcome text-center">
+            {' '}
+            Ooops! <span>😯</span> This movie was not found
+          </Title>
+          <Title level={3}>
+            Please check if you typed your movie Title Correctly or search for another movie <span>😊</span>{' '}
+          </Title>
+        </Space>
+      </Row>
+    );
   } else {
     return (
-      <Row> <Space direction="vertical">
-        <Title level={1} className="text-welcome text-center" > Welcome to your Movie Library</Title>
-        <Title level={3}>Type your favourite movie UP in a searchbar and create your Movie Library</Title>
-      </Space></Row>)
+      <Row>
+        <Space direction="vertical">
+          <Title level={1} className="text-welcome text-center">
+            Welcome to your Movie Library
+          </Title>
+          <Title level={3}>Type your favourite movie UP in a searchbar and create your Movie Library</Title>
+        </Space>
+      </Row>
+    );
   }
-
-}
-export default MovieInfo
+};
+export default MovieInfo;
